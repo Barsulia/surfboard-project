2862e07250f616229b2965b6f33238b5a5b4ee69
Iuliia Barsukova <glazgotens82@mail.ru>
2021-05-17 22:25:47 +0300

    changes for 3 week

--- js/fullscreen-menu.js
+++ js/fullscreen-menu.js
@@ -1,12 +1,16 @@
 const hamburger = document.querySelector(".hamburger");
 const fullscreenMenu = document.querySelector(".fullscreen-menu");
 const width = document.documentElement.clientWidth;
+const closeButton = document.querySelector(".fullscreen-menu__close")
 
 if (width <= 768) {
   hamburger.addEventListener("click", e => {
-    console.log('клик по кнопке');
     e.preventDefault();
-    fullscreenMenu.style.display = "flex";
     fullscreenMenu.classList.add('active'); 
   })
 }
+
+closeButton.addEventListener("click", e => {
+  e.preventDefault();
+  fullscreenMenu.classList.remove('active');
+})
