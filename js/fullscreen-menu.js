a0f3be9a2f7b6f2e52b29fdc029b05438c710d7e
Iuliia Barsukova <glazgotens82@mail.ru>
2021-05-19 22:14:54 +0300

    убран скролл при открытом модально окне

--- js/fullscreen-menu.js
+++ js/fullscreen-menu.js
@@ -1,16 +1,20 @@
 const hamburger = document.querySelector(".hamburger");
 const fullscreenMenu = document.querySelector(".fullscreen-menu");
 const width = document.documentElement.clientWidth;
-const closeButton = document.querySelector(".fullscreen-menu__close")
+const closeButton = document.querySelector(".fullscreen-menu__close");
+const body = document.querySelector("body");
 
 if (width <= 768) {
   hamburger.addEventListener("click", e => {
     e.preventDefault();
-    fullscreenMenu.classList.add('active'); 
-  })
-}
+    fullscreenMenu.classList.add('active');
+    body.style.overflow = 'hidden';
+  });
+};
 
 closeButton.addEventListener("click", e => {
   e.preventDefault();
   fullscreenMenu.classList.remove('active');
-})
+  body.style.overflow = '';
+});
+
